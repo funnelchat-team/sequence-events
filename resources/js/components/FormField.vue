c55fedd1ea725f2f4d7e74b0a0c4bb9abcbb72aa
Marce <marcelo.marin.salvagiot@gmail.com>
2021-01-07 11:31:29 -0300

    fix time zone

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,97 +1,106 @@
 <template>
-  <default-field :field="field" :errors="errors">
-    <template slot="field">
-      <div>
-        <form class="w-full max-w-sm">
-          <div class="md:flex md:items-center mb-6">
-            <div class="md:w-1/3">
-              <label
-                class="inline-block text-80 pt-2 leading-tight"
-                for="inline-full-name"
-              >
-                Type
-              </label>
-            </div>
-            <div class="md:w-2/3">
-              <multiselect v-model="type_selected" :options="options">
-              </multiselect>
-            </div>
-          </div>
+    <default-field :field="field" :errors="errors">
+        <template slot="field">
+            <div>
+                <form class="w-full max-w-sm">
+                    <div class="md:flex md:items-center mb-6">
+                        <div class="md:w-1/3">
+                            <label
+                                class="inline-block text-80 pt-2 leading-tight"
+                                for="inline-full-name"
+                            >
+                                {{ __("Type") }}
+                            </label>
+                        </div>
+                        <div class="md:w-2/3">
+                            <multiselect
+                                v-model="type_selected"
+                                :options="options"
+                            >
+                            </multiselect>
+                        </div>
+                    </div>
 
-          <div v-if="type_selected == 'Duration'">
-            <div class="md:flex md:items-center mb-6">
-              <div class="md:w-1/3">
-                <label
-                  class="inline-block text-80 pt-2 leading-tight"
-                  for="inline-full-name"
-                >
-                  Duration
-                </label>
-              </div>
-              <div class="md:w-2/3">
-                <multiselect
-                  v-model="selected_duration_type"
-                  :options="duration_type_collect"
-                ></multiselect>
-              </div>
+                    <div v-if="type_selected == 'Duration'">
+                        <div class="md:flex md:items-center mb-6">
+                            <div class="md:w-1/3">
+                                <label
+                                    class="inline-block text-80 pt-2 leading-tight"
+                                    for="inline-full-name"
+                                >
+                                    {{ __("Duration") }}
+                                </label>
+                            </div>
+                            <div class="md:w-2/3">
+                                <multiselect
+                                    v-model="selected_duration_type"
+                                    :options="duration_type_collect"
+                                ></multiselect>
+                            </div>
+                        </div>
+                        <div class="md:flex md:items-center mb-6">
+                            <div class="md:w-1/3">
+                                <label
+                                    class="inline-block text-80 pt-2 leading-tight"
+                                    for="inline-full-name"
+                                >
+                                    {{ __("Time") }}
+                                </label>
+                            </div>
+                            <div class="md:w-2/3">
+                                <input
+                                    v-model="selected_cant"
+                                    type="number"
+                                    class="w-full form-control form-input form-input-bordered"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div v-if="type_selected == 'Day is'">
+                        <div class="md:flex md:items-center mb-6">
+                            <div class="md:w-1/3">
+                                <label
+                                    class="inline-block text-80 pt-2 leading-tight"
+                                    for="inline-full-name"
+                                >
+                                    {{ __("Day is") }}
+                                </label>
+                            </div>
+                            <div class="md:w-2/3">
+                                <multiselect
+                                    v-model="selected_day_is"
+                                    :options="day_is_collect"
+                                ></multiselect>
+                            </div>
+                        </div>
+                        <div class="md:flex md:items-center mb-6">
+                            <div class="md:w-1/3">
+                                <label
+                                    class="inline-block text-80 pt-2 leading-tight"
+                                    for="inline-full-name"
+                                >
+                                    {{ __("Time") }}
+                                </label>
+                            </div>
+                            <div class="md:w-2/3">
+                                <!-- <div>
+                  <flat-pickr v-model="selected_time"></flat-pickr>
+                </div> -->
+                                <input
+                                    v-model="selected_time"
+                                    type="time"
+                                    class="w-full form-control form-input form-input-bordered"
+                                />
+                                <span class="text-80 text-sm ml-2">{{
+                                    resolved_options.timeZone
+                                }}</span>
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </div>
-            <div class="md:flex md:items-center mb-6">
-              <div class="md:w-1/3">
-                <label
-                  class="inline-block text-80 pt-2 leading-tight"
-                  for="inline-full-name"
-                >
-                  Time
-                </label>
-              </div>
-              <div class="md:w-2/3">
-                <input
-                  v-model="selected_cant"
-                  type="number"
-                  class="w-full form-control form-input form-input-bordered"
-                />
-              </div>
-            </div>
-          </div>
-          <div v-if="type_selected == 'Day is'">
-            <div class="md:flex md:items-center mb-6">
-              <div class="md:w-1/3">
-                <label
-                  class="inline-block text-80 pt-2 leading-tight"
-                  for="inline-full-name"
-                >
-                  Day is
-                </label>
-              </div>
-              <div class="md:w-2/3">
-                <multiselect
-                  v-model="selected_day_is"
-                  :options="day_is_collect"
-                ></multiselect>
-              </div>
-            </div>
-            <div class="md:flex md:items-center mb-6">
-              <div class="md:w-1/3">
-                <label
-                  class="inline-block text-80 pt-2 leading-tight"
-                  for="inline-full-name"
-                >
-                  Time
-                </label>
-              </div>
-              <div class="md:w-2/3">                
-                <input
-                  v-model="selected_time"
-                  type="time"
-                  class="w-full form-control form-input form-input-bordered"
-                />
-              </div>
-            </div>
-          </div>
-        </form>
-      </div>
-    </template>
-  </default-field>
+        </template>
+    </default-field>
 </template>
 
 <script>
@@ -101,96 +110,109 @@ import flatPickr from "vue-flatpickr-component";
 import "flatpickr/dist/flatpickr.css";
 
 export default {
-  mixins: [FormField, HandlesValidationErrors],
+    mixins: [FormField, HandlesValidationErrors],
 
-  components: { Multiselect, flatPickr },
+    components: { Multiselect, flatPickr },
 
-  data() {
-    return {
-      time_value: null,
-      options: ["Day is", "Duration"],
-      day_is_collect: [
-        "monday",
-        "tuesday",
-        "wednesday",
-        "thursday",
-        "friday",
-        "saturday",
-        "sunday",
-        "tomorrow",
-      ],
-      duration_type_collect: ["seconds", "minutes", "hour", "day"],
-      type_selected: "Day is",
-      selected_day_is: null,
-      selected_time: null,
-      selected_duration_type: null,
-      selected_cant: null,
-    };
-  },
-  props: ["resourceName", "resourceId", "field"],
-
-  watch: {
-    selected_day_is: function(newDay, oldDay) {
-      this.value = JSON.stringify({
-        type_selected: this.type_selected,
-        selected_day_is: newDay,
-        selected_time: "00:00",
-      });
+    data() {
+        return {
+            time_value: null,
+            options: ["Day is", "Duration"],
+            day_is_collect: [
+                "sunday",
+                "monday",
+                "tuesday",
+                "wednesday",
+                "thursday",
+                "friday",
+                "saturday"
+            ],
+            duration_type_collect: ["seconds", "minutes", "hour", "day"],
+            type_selected: "Day is",
+            selected_day_is: this.getDay(),
+            selected_time: "00:00",
+            selected_duration_type: null,
+            selected_cant: null,
+            resolved_options: Intl.DateTimeFormat().resolvedOptions()
+        };
     },
-    selected_time: function(newTime, oldTime) {
-      this.value = JSON.stringify({
-        type_selected: this.type_selected,
-        selected_day_is: this.selected_day_is,
-        selected_time: newTime,
-      });
-    },
-    selected_duration_type: function(newHour, oldHour) {
-      this.value = JSON.stringify({
-        type_selected: this.type_selected,
-        selected_duration_type: newHour,
-        selected_cant: 1,
-      });
-    },
-    selected_cant: function(newCant, oldCant) {
-      this.value = JSON.stringify({
-        type_selected: this.type_selected,
-        selected_duration_type: this.selected_duration_type,
-        selected_cant: newCant,
-      });
-    },
-  },
+    props: ["resourceName", "resourceId", "field"],
 
-  mounted() {
-    console.log(this.field.value);
-  },
-
-  methods: {
-    inputHandler(eventData) {
-      console.log(eventData);
-    },
-
-    setInitialValue() {
-      if (this.field.value) {
-        let val = JSON.parse(this.field.value);
-        if (val.type_selected == "Day is") {
-          this.type_selected = val.type_selected;
-          this.selected_day_is = val.selected_day_is;
-          this.selected_time = val.selected_time;
-        } else {
-          this.type_selected = val.type_selected;
-          this.selected_duration_type = val.selected_duration_type;
-          this.selected_cant = val.selected_cant;
+    watch: {
+        selected_day_is: function(newDay, oldDay) {
+            this.value = JSON.stringify({
+                type_selected: this.type_selected,
+                selected_day_is: newDay,
+                selected_time: "00:00"
+            });
+        },
+        selected_time: function(newTime, oldTime) {
+            this.value = JSON.stringify({
+                type_selected: this.type_selected,
+                selected_day_is: this.selected_day_is,
+                selected_time: newTime + " " + this.resolved_options.timeZone
+            });
+        },
+        selected_duration_type: function(newHour, oldHour) {
+            this.value = JSON.stringify({
+                type_selected: this.type_selected,
+                selected_duration_type: newHour,
+                selected_cant: 1
+            });
+        },
+        selected_cant: function(newCant, oldCant) {
+            this.value = JSON.stringify({
+                type_selected: this.type_selected,
+                selected_duration_type: this.selected_duration_type,
+                selected_cant: newCant
+            });
         }
-      }
     },
 
-    fill(formData) {
-      formData.append(this.field.attribute, this.value || "");
+    mounted() {
+        //this.getDay();
     },
 
-    handleChange(value) {
-      this.value = value;
-    },
-  },
+    methods: {
+        inputHandler(eventData) {
+            //console.log(eventData);
+        },
+
+        getDay() {
+            let days = [
+                "sunday",
+                "monday",
+                "tuesday",
+                "wednesday",
+                "thursday",
+                "friday",
+                "saturday"
+            ];
+            return days[new Date().getDay()];
+        },
+
+        setInitialValue() {
+            if (this.field.value) {
+                let val = JSON.parse(this.field.value);
+                if (val.type_selected == "Day is") {
+                    this.type_selected = val.type_selected;
+                    this.selected_day_is = val.selected_day_is;
+                    this.selected_time = val.selected_time.split(" ")[0];
+                } else {
+                    this.type_selected = val.type_selected;
+                    this.selected_duration_type = val.selected_duration_type;
+                    this.selected_cant = val.selected_cant;
+                }
+            }
+        },
+
+        fill(formData) {
+            formData.append(this.field.attribute, this.value || "");
+        },
+
+        handleChange(value) {
+            this.value = value;
+        }
+    }
 };
 </script>
