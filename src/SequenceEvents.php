8e1d4b9329658907164970ca691a9d0b55c387e5
Horacio Alejandro Kuszniruk <horacio.kuszniruk@gmail.com>
2021-01-28 09:11:54 -0300

    se agregó la funcionalidad de la traducción

--- src/SequenceEvents.php
+++ src/SequenceEvents.php
@@ -12,4 +12,30 @@ class SequenceEvents extends Field
      * @var string
      */
     public $component = 'sequence-events';
+
+    public function translate()
+    {
+        return $this->withMeta([
+            'day_is_collect' => [
+                ['data' => 'monday', 'label' => __('Monday')],
+                ['data' => 'tuesday', 'label' =>  __('Tuesday')],
+                ['data' => 'wednesday', 'label' => __('Wednesday')],
+                ['data' => 'thursday', 'label' => __('Thursday')],
+                ['data' => 'friday', 'label' => __('Friday')],
+                ['data' => 'saturday', 'label' => __('Saturday')],
+                ['data' => 'sunday', 'label' => __('Sunday')],
+                ['data' => 'tomorrow', 'label' => __('Tomorrow')],
+            ],
+            'duration_type_collect' => [
+                ['data' => 'seconds', 'label' => __('Seconds')],
+                ['data' => 'minutes', 'label' => __('Minutes')],
+                ['data' => 'hour', 'label' => __('Hours')],
+                ['data' => 'day', 'label' => __('Days')],
+            ],
+            'options' => [
+                ['data' => 'Day is', 'label' => __('Day is')],
+                ['data' => 'Duration', 'label' => __('Duration type')],
+            ]
+        ]);
+    }
 }
